abbc3b97ab7a3b41e694c16bd67307ba9656a263
limin.ling <limin.ling@renren-inc.com>
2017-03-03 16:00:54 +0800

    test

--- test/src/js/index.js
+++ test/src/js/index.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
-var app = new Vue({
+
+Vue.component('my-component', {
+  template: '<div>A custom component!</div>'
+})
+
+var app1 = new Vue({
   el: '#app',
   data: {
     todos: [
       { text: 'Learn JavaScript' },
       { text: 'Learn Vue.js' },
       { text: 'Build Something Awesome' }
-    ]
+    ],
+    title: "titleee",
+    ok: true
+  },
+  methods: {
+  	reverseType: function(){
+  		this.ok = false
+  	}
   }
 })
+app1.title=111;
 require("../sass/index.less");
